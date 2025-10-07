be21ec6837ef26cef5f47509f8b24c04695be3f4
voltaan <voltaan@proton.me>
2025-10-07 16:31:01 +0200

    Removed tester.py and updated user interface

--- gui.py
+++ gui.py
@@ -48,7 +48,64 @@ class Gui(QMainWindow):
         QMessageBox.information(self, "About facce", "facce version testing \nhttps://github.com/voltaan/facce")
 
     def fd_webcam_dialog(self):
-        return 0
+        faceDetectionOptions = QDialog(self)
+        faceDetectionOptions.setFixedSize(500, 400)
+        faceDetectionOptions.setWindowTitle("face detection options")
+
+        # Main layout
+        layouts = QWidget(faceDetectionOptions)
+        layout = QVBoxLayout(layouts)
+
+        # Width
+        widthInput = self.add_label_and_input(layout, "Width",
+                                              "If unsure, set to the maximum width resolution of your webcam (e.g., 1280)")
+
+        # Height
+        heightInput = self.add_label_and_input(layout, "Height",
+                                               "If unsure, set to the maximum height resolution of your webcam (e.g., 720)")
+
+        # Flip
+        flipInput = self.add_label_and_input(layout, "Flip", "If unsure, set to +1")
+
+        # Scale factor
+        scaleFactorInput = self.add_label_and_input(layout, "Scale factor", "If unsure, set to 1.0")
+
+        # Minimum neighbors
+        self.add_label_and_input(layout, "Minimum neighbors", "If unsure, set to 5")
+
+        # Color
+        colorLayout = QHBoxLayout()
+
+        redInput = QLineEdit("255")
+        redInput.setPlaceholderText("Red")
+        colorLayout.addWidget(redInput)
+
+        greenInput = QLineEdit("0")
+        greenInput.setPlaceholderText("Green")
+        colorLayout.addWidget(greenInput)
+
+        blueInput = QLineEdit("0")
+        blueInput.setPlaceholderText("Blue")
+        colorLayout.addWidget(blueInput)
+
+        # Add color layout to main layout
+        layout.addLayout(colorLayout)
+
+        # Color label
+        colorLabel = QLabel("Select the color (RGB) with you want to mark the detected face.")
+        layout.addWidget(colorLabel)
+
+        faceDetectionOptions.setLayout(layout)
+        faceDetectionOptions.show()
+
+
+    def add_label_and_input(self, layout, placeholder, message):
+        input_field = QLineEdit("")
+        input_field.setPlaceholderText(placeholder)
+        label = QLabel(message)
+        layout.addWidget(input_field)
+        layout.addWidget(label)
+        return input_field
 
     def fd_webcam_eyes(self):
         return 0
