e06b4643af3f18ffa6f5fcb9cd2ad946bff5c479
voltaan <voltaan@proton.me>
2025-10-07 18:12:08 +0200

    Added smile detection

--- faceDetection.py
+++ faceDetection.py
@@ -2,8 +2,10 @@ import cv2
 
 faceCascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_default.xml')
 eyeCascade = cv2.CascadeClassifier('haar/haarcascade_eye.xml')
+smileCascade = cv2.CascadeClassifier('haar/haarcascade_smile.xml')
 
-def detectFace(width, height, flip=+1, red=255, green=0, blue=0, scaleFactor=1.2, minNeighbors=5,):
+
+def detectFace(width, height, flip=+1, red=255, green=0, blue=0, scaleFactor=1.2, minNeighbors=5, ):
     cap = cv2.VideoCapture(0)
     cap.set(3, width)
     cap.set(4, height)
@@ -34,8 +36,9 @@ def detectFace(width, height, flip=+1, red=255, green=0, blue=0, scaleFactor=1.2
     cv2.destroyAllWindows()
 
 
-def detectEye(width, height, flip=+1, scaleFactor=1.2, minNeighbors=5, eyeScaleFactor=1.5, eyeMinNeighbors=10, red=255,
-              green=0, blue=0, eyeRed=0, eyeGreen=255, eyeBlue=0):
+def detectEye(width, height, flip=+1, red=255,
+              green=0, blue=0, eyeRed=0, eyeGreen=255, eyeBlue=0, scaleFactor=1.2, minNeighbors=5, eyeScaleFactor=1.5,
+              eyeMinNeighbors=10):
     cap = cv2.VideoCapture(0)
     cap.set(3, width)
     cap.set(4, height)
@@ -71,3 +74,41 @@ def detectEye(width, height, flip=+1, scaleFactor=1.2, minNeighbors=5, eyeScaleF
             break
     cap.release()
     cv2.destroyAllWindows()
+
+
+def detectSmile(width, height, flip=+1, red = 255, green = 0, blue = 0, smileRed=0, smileGreen=255, smileBlue=0, scaleFactor=1.3, minNeighbors=5, smileScaleFactor=1.5, smileMinNeighbors=15):
+    cap = cv2.VideoCapture(0)
+    cap.set(3, width)
+    cap.set(4, height)
+
+    while True:
+        ret, img = cap.read()
+        img = cv2.flip(img, flip)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=scaleFactor,
+            minNeighbors=minNeighbors,
+            minSize=(30,30)
+        )
+
+        for(x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (red,green,blue), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+
+            smile = smileCascade.detectMultiScale(
+                roi_gray,
+                scaleFactor=smileScaleFactor,
+                minNeighbors=smileMinNeighbors,
+                minSize=(25,25)
+            )
+
+            for(xx,yy,ww,hh) in smile:
+                cv2.rectangle(roi_color, (xx,yy), (xx+ww, yy+hh),(smileRed,smileGreen,smileBlue),2)
+
+        cv2.imshow('smile detection - press esc to exit', img)
+
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
---
+++ tester.py
@@ -0,0 +1,3 @@
+import faceDetection
+
+faceDetection.detectSmile(1280,720)
